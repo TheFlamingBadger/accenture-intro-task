bd7caabbb0c59a116c0b108e45bd03cd1a644f7f
Justine Xue <jxue5194@uni.sydney.edu.au>
2025-06-26 14:10:03 +1000

    updating first script

---
+++ restcountries.py
@@ -0,0 +1,7 @@
+# 1. Use [this API](https://restcountries.com/) to:
+
+# - List the 10 countries with the highest population.
+# - Find the countries in the Southern Hemisphere (lat/lng).
+# - Find the country with the longest name.
+# - List all countries that speak Spanish.
+# - Calculate the average population across all countries.
