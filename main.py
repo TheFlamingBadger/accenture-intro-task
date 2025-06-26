d4aa9754994d24c55e555dda7a2cb0a6be63529e
Angus McLean <naelcmsugna@gmail.com>
2025-06-26 14:14:16 +1000

    Add basic functions for task 4

---
+++ main.py
@@ -0,0 +1,5 @@
+# ./countrytool save --format json|csv --output countries.json — save all countries to a file
+# ./countrytool help — show help text
+
+def main():
+    return 0
---
+++ queries.py
@@ -0,0 +1,11 @@
+# ./countrytool top-population [n] — show top n countries by population
+def getByDescPopulation(n):
+    return
+
+# ./countrytool language [lang] — show countries that speak a given language
+def getByLanguage(lang):
+    return
+
+# ./countrytool southern — list countries in the Southern Hemisphere
+def getByHemisphere(hemisphere):
+    return
