524cf2fb93da323019c5dc119e4ded35a10570bb
Justine Xue <jxue5194@uni.sydney.edu.au>
2025-06-26 14:20:53 +1000

    Updating

--- queries.py
+++ queries.py
@@ -2,22 +2,27 @@
 api_url_1 = "https://restcountries.com/v3.1/all"
 
 # ./countrytool top-population [n] — show top n countries by population
+#JUSTINE
 def getByDescPopulation(n):
     return
 
 # ./countrytool language [lang] — show countries that speak a given language
+#JUSTINE
 def getByLanguage(lang):
     return
 
 # ./countrytool southern — list countries in the Southern Hemisphere
+#JUSTINE
 def getByHemisphere(hemisphere):
     return
 
 # ./countrytool longest-name — find the country with the longest name
+#QUENTON
 def getByLongestName():
     return
 
 # ./countrytool average-population — calculate the average population across all countries
+#QUENTON
 def getAveragePopulation():
     return
 
