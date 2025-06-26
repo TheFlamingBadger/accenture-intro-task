c50625f5ab1b86c6a7e99ed7089b9b796f1e4af3
Angus McLean <naelcmsugna@gmail.com>
2025-06-26 15:38:36 +1000

    Update help string

--- constants.py
+++ constants.py
@@ -5,12 +5,15 @@ unknownCmdString = "Unrecognised command. Use 'help' to see available commands."
 helpString = """
 Available Commands:
 
-1. save --format [json|csv] --output [name].json — save all countries to s file
-2. [southern/northern] — list countries in specified hemisphere.
-3. longest-name — list country with the longest name
-4. avg-population — show avg population of all countries
-5. temperature [lat] [lon] - show current temperature at specified latitude/longitude
-6. precipitation [lat] [lon] - show current temperature at specified latitude/longitude
+1. help — list commands
+2. top-population [n] — show top n countries by population
+3. language [lang] — show countries that speak a given language
+4. save --format json|csv --output countries.json — save all countries to a file
+5. hemisphere [southern|northern] — list countries in the Southern Hemisphere
+6. longest-name — list country with the longest name
+7. avg-population — show avg population of all countries
+8. temperature [lat] [lon] - show current temperature of location accessed via latitude/longitude
+9. precipitation [lat] [lon] - show current temperature of location accessed via latitude/longitude
 """
 
 hemishperes = ["northern", "southern"]
